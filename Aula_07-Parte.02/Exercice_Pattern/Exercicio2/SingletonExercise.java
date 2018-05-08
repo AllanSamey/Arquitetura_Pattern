62f4960b75c6dc3d51dfbb842ec187dd0e5b5280
Alunos <Alunos@mocli10238.animaedu.intranet>
2018-05-08 19:03:47 -0300

    Aula_07-Parte:2

---
+++ Aula_07-Parte.02/Exercice_Pattern/Exercicio2/SingletonExercise.java
@@ -0,0 +1,16 @@
+package Exercicio2;
+
+/**
+ * 
+ * @author Allan Samey Cordeiro Ramos - RA:201516605 - Turma:SIN3AN-MCA
+ * 			
+ *
+ */
+public class SingletonExercise {
+	public static void main(String args[]) {
+		Deck deck = new Deck( );
+		
+		deck.print( );
+		}
+
+}
